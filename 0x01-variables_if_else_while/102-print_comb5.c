c5fc6effb18481902d0b9159a1d2c161c644b39b
Isaac Frank <codingwithisaac@gmail.com>
2023-03-17 20:04:51 +0100

    inner loop should start from outter checkpoint

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num1 = 0; num1 <  99; num1++) /** Ignore 9 as 90 and 09 are same */
 	{
-		for (num2 = 0; num2 <  99; num2++)
+		for (num2 = num1; num2 <  99; num2++)
 		{
 			if (num1 == num2)
 				continue;
