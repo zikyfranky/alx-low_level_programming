94e05cd60e41e6f797be8095c87fe2958d268de6
Isaac Frank <codingwithisaac@gmail.com>
2023-03-17 18:44:57 +0100

    Print Numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - The Main Function
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <  10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
