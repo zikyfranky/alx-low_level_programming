69278d5b903606a1a087885cd0fbaaa7616ebcc9
Isaac Frank <codingwithisaac@gmail.com>
2023-03-17 21:05:25 +0100

    Get all combination of 2, 2 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,24 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <  99; num1++) /** Ignore 9 as 90 and 09 are same */
+	for (num1 = 0; num1 <  100; num1++) /** Ignore 9 as 90 and 09 are same */
 	{
-		for (num2 = num1; num2 <  99; num2++)
+		for (num2 = num1; num2 <  100; num2++)
 		{
-			if (num1 == num2)
-				continue;
+			if (num1 < num2)
+			{
+				putchar(48 + (num1 / 10));
+				putchar(48 + (num1 % 10));
+				putchar(' ');
+				putchar(48 + (num2 / 10));
+				putchar(48 + (num2 % 10));
 
-			putchar(48 + (num1 / 10));
-			putchar(48 + (num1 % 10));
-			putchar(' ');
-			putchar(48 + (num2 / 10));
-			putchar(48 + (num2 % 10));
-
-			if (num1 == 98 && num2 == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 
