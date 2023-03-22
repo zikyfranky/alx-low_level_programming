58faa808d2bbe3544903df6d11d3141ad63a1ea3
Isaac Frank <codingwithisaac@gmail.com>
2023-03-22 17:01:18 +0100

    Fibonacci Sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point - prints first 50 Fib Sequence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long num1 = 0, num2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = num1 + num2;
+		printf("%ld", sum);
+
+		num1  = num2;
+		num2 = sum;
+
+		printf("%s", count == 49 ? "\n" : ",");
+	}
+
+	return (0);
+}
