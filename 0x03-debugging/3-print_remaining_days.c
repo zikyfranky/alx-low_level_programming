5360ff13f58eb188c8bdf3a74a04be2321016bf2
Isaac Frank <codingwithisaac@gmail.com>
2023-03-22 19:14:19 +0100

    Redundant print statement

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,6 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
 {
-printf("It's a LEAP year\n");
 if (month >= 2 && day >= 60)
 {
 day++;
