3ff2a9c0973ec9061f51f2a7256011671f5ee12c
Isaac Frank <codingwithisaac@gmail.com>
2023-03-24 00:10:57 +0100

    Multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiply @a with @b
+ * @a: First number
+ * @b: Number to be multiplied by
+ *
+ * Return: The product of @a and @b
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
