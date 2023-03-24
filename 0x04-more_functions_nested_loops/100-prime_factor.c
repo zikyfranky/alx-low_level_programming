07ab58c38bf49ae12c87845c89389545bdfb57c8
Isaac Frank <codingwithisaac@gmail.com>
2023-03-24 03:42:24 +0100

    Betty styling

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,17 +8,20 @@
 
 int main(void)
 {
-    long long i, n = 612852475143;
+long long i, n = 612852475143;
 
-    for (i = 2; i <= sqrt(n); i++) {
-        while (n % i == 0) {
-            n /= i;
-        }
-    }
+for (i = 2; i <= sqrt(n); i++)
+{
+while (n % i == 0)
+{
+n /= i;
+}
+}
 
-    if (n > 1) {
-        printf("%lld\n", n);
-    }
+if (n > 1)
+{
+printf("%lld\n", n);
+}
 
-    return (0);
+return (0);
 }
