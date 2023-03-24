8faf2eff13b843a19da1ef6efa0c95b69904121d
Isaac Frank <codingwithisaac@gmail.com>
2023-03-24 03:40:26 +0100

    Get largest Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor of the number 612852475143
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+    long long i, n = 612852475143;
+
+    for (i = 2; i <= sqrt(n); i++) {
+        while (n % i == 0) {
+            n /= i;
+        }
+    }
+
+    if (n > 1) {
+        printf("%lld\n", n);
+    }
+
+    return (0);
+}
