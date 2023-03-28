8ca49ed72df2ec0d8917677ba07d3d4543ac58fe
Isaac Frank <codingwithisaac@gmail.com>
2023-03-28 04:03:32 +0100

    Print second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - Prints half of the string
+ *
+ * @str: The string to print half of
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i, len = strlen(str);
+	/*"Hello" -> (5-1)/2 -> 4/2 -> 2 -> "llo"*/
+	/*"Hell" -> 4/2 -> 2 -> "ll"*/
+	int startFrom = len % 2 == 0 ? len / 2 : (len - 1) / 2;
+
+	for (i = startFrom; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
