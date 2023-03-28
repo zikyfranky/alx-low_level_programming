cc5cff7f642643ba50c96347b39f05dbc3a7a021
Isaac Frank <codingwithisaac@gmail.com>
2023-03-28 03:05:04 +0100

    Switch values from two variables + header file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - Takes two arguments and swap their values with pointers
+ *
+ * @a: Pointer to first argument
+ * @b: Pointer to second argument
+ */
+
+void swap_int(int *a, int *b)
+{
+int holder;
+
+holder = *a;
+*a = *b;
+*b = holder;
+}
