a113dd7d48e8d0e3cf1ad2794441ac01aa6e583b
Isaac Frank <codingwithisaac@gmail.com>
2023-03-28 04:14:32 +0100

    Fix proper arithmetics for second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,13 @@
 void puts_half(char *str)
 {
 	int i, len = strlen(str);
-	/*"Hello" -> (5-1)/2 -> 4/2 -> 2 -> "llo"*/
+	/*"Hello" -> ((5-1)/2)+1 -> (4/2)+1 -> 2+1 -> 3 -> "lo"*/
 	/*"Hell" -> 4/2 -> 2 -> "ll"*/
-	int startFrom = len % 2 == 0 ? len / 2 : (len - 1) / 2;
+	int startFrom = len % 2 == 0 ? len / 2 : ((len - 1) / 2) + 1;
 
 	for (i = startFrom; i < len; i++)
 	{
-		_putchar(str[i]);
+		_putchar("%c", str[i]);
 	}
-	_putchar('\n');
+	_putchar("%c", '\n');
 }
