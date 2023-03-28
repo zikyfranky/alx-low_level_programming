be6ad939dc2f4042f2d21190d31c16be85ce7039
Isaac Frank <codingwithisaac@gmail.com>
2023-03-28 03:23:05 +0100

    Reverse string and print out

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Takes a string and prints it backwards to stdout
+ *
+ * @str: The string to reverse and print out
+ *
+ */
+
+void print_rev(char *str)
+{
+	int len = _strlen(str);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
