1c7605ebcd74688348b024eec3c4944dd20fc474
Isaac Frank <codingwithisaac@gmail.com>
2023-03-28 03:09:15 +0100

    Fix betty standard

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,9 +4,11 @@
  * _strlen - Takes a string and returns the length of the string
  *
  * @s: The string to get the length of
+ * 
+ * Return: The length of the string
  */
 
 int _strlen(char *s)
 {
-return strlen(s);
+return (strlen(s));
 }
