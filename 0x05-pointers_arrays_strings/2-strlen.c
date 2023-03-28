fd00c1412df2f2304b1f1428d6411965fcb995db
Isaac Frank <codingwithisaac@gmail.com>
2023-03-28 03:10:47 +0100

    Include string header file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <string.h>
 
 /**
  * _strlen - Takes a string and returns the length of the string
  *
  * @s: The string to get the length of
- * 
+ *
  * Return: The length of the string
  */
 
