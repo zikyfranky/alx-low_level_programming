c125a013616009f55a18c6c98f14789502bb4805
Isaac Frank <codingwithisaac@gmail.com>
2023-03-30 21:46:45 +0100

    Proper doc-style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,11 @@
 #include <string.h>
 
 /**
- * _strncat - Appends @src to @dest
+ * _strncat - Appends @src to @dest up to @n characters from @src
  *
  * @dest: String to be modified
  * @src: String to append to @dest
+ * @n: Max number of characters to append
  *
  * Return: Pointer to the resulting string @dest
  */
