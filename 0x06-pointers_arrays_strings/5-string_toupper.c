e199f8456bb3e0822c2e21ba2ef0f824f00ca1ab
Isaac Frank <codingwithisaac@gmail.com>
2023-03-31 02:16:25 +0100

    replace all lowercases with uppercases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * string_toupper - Convert all lowercase characters to upper
+ *
+ * @s: String to be modified
+ *
+ * Return: Pointer to the modified string
+ */
+
+char *string_toupper(char *s)
+{
+	int i, len = strlen(s);
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			/*
+			 * Lowercase characters are -32 of upper case characters in the ASCII table
+			 * so we need to convert them to upper case by subtracting 32
+			 */
+			s[i] -= 32;
+		}
+	}
+
+	return (s);
+}
