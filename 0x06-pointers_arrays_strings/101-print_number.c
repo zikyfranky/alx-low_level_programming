72003ebd93ddcedf6069ce886054b79f25668c86
Isaac Frank <codingwithisaac@gmail.com>
2023-04-01 00:21:11 +0100

    Recursively print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Function that prints an integer
+ * @n: The integer number
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
