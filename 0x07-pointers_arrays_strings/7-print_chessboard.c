2aa590cdff8de9f04670382286ab7cccd0b3528e
Isaac Frank <codingwithisaac@gmail.com>
2023-04-06 12:19:39 +0100

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <string.h>
+
+/**
+ * print_chessboard - Prints the current state of the chessboard
+ *
+ * @a: 2-dimensional array
+ *
+ * Return: No return value
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
