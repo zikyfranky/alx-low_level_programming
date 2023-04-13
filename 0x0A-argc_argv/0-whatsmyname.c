5a2695eaaf3e63d2a9e82f038d98efa41cb9a0ec
Isaac Frank <codingwithisaac@gmail.com>
2023-04-13 17:47:08 +0100

    Print program filename and ignore unused parameter

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Length of the command line arguments
+ * @argv: Command line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
