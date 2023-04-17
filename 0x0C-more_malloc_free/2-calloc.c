d39a978f076d081817b978feee55e339534c6e08
Isaac Frank <codingwithisaac@gmail.com>
2023-04-17 19:56:13 +0100

    Create a custom calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory
+ * @nmemb: Number of members to allocate
+ * @size: Memory size
+ *
+ * Return: pointer to array (SUCCESS), NULL(ERROR)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int **array;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	/*Create memory needed for the height */
+	array = malloc(size * nmemb);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
+
+	return (array);
+}
