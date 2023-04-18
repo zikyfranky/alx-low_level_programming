02ab4717fdf855ae17cf2379a8528f4eb2dedc66
Isaac Frank <codingwithisaac@gmail.com>
2023-04-18 15:48:22 +0100

    Betty docs and return value

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints the name of the source file
+ *
+ * Return: 0
+ */
+int main(void)
 {
-    printf("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
+	return (0);
 }
