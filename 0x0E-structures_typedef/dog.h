d8b7b92b5fa5b37950bf2b769da2e29ba4498ab9
Isaac Frank <codingwithisaac@gmail.com>
2023-04-18 17:38:48 +0100

    Betty docs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - a dog's attribute
+ * @name: First memeber - Name of the dog
+ * @age: Second member - Age of the dog
+ * @owner: Third member - Owner of the dog
+ *
+ */
 struct dog
 {
 	char *name;
