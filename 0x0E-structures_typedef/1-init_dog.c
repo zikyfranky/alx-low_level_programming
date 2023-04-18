f72df8300c7ea6dbf6e644115c8a8e5fd7ad4452
Isaac Frank <codingwithisaac@gmail.com>
2023-04-18 18:33:34 +0100

    DO NOT CREATE MEMORY

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -12,10 +13,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-
 	if (d == NULL)
 		return;
 
