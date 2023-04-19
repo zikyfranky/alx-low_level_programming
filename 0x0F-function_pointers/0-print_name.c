b61dee8ef55aff607344c50bb91bb5a81eb3e928
Isaac Frank <codingwithisaac@gmail.com>
2023-04-19 13:35:22 +0100

    Print name using callback function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - Prints the name variable
+ * @name: the name to print
+ * @f: A function pointer to call and pass @name to
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
