44a4e61f514e46b49e389eac3066f1dc4167ed99
Isaac Frank <codingwithisaac@gmail.com>
2023-04-21 01:42:38 +0100

    Do not modify separator variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,9 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator == NULL)
-		separator = "";
-
 	/**
 	 * man stdarg -> Docs
 	 * va_start -> copies start point of @n to va_list args
@@ -29,11 +26,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (i < (n - 1))
+		if (i < (n - 1) && separator)
 			printf("%s", separator);
-		else
-			printf("\n");
 	}
+
+	printf("\n");
 
 	va_end(args);
 }
