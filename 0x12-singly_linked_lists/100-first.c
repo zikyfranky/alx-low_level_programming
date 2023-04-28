e1674d712e46a093186040b0a31266d0ff36742c
Isaac Frank <codingwithisaac@gmail.com>
2023-04-28 03:19:15 +0100

    Betty docs fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 
 /**
- * constructor - Run code first
+ * construc_func - Run code first
  */
-
 void __attribute__((constructor)) construc_func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
