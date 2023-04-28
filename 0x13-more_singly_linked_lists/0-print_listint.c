10700bdcc6b2ca3baf4ec3af7bf654cbb8ba7bbc
Isaac Frank <codingwithisaac@gmail.com>
2023-04-28 23:57:19 +0100

    Get number of nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all element in a linked list
+ *
+ * @h: Start Node
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *new;
+	listint_t hello = {8, NULL};
+	size_t n;
+
+	head = &hello;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	new->next = head;
+	head = new;
+	n = print_listint(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (0);
+}
