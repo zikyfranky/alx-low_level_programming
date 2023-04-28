12a6495da27169df4a46e7ff466a28a707b3ff0a
Isaac Frank <codingwithisaac@gmail.com>
2023-04-28 03:18:11 +0100

    Creating constructors in C

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * constructor - Run code first
+ */
+
+void __attribute__((constructor)) construc_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
