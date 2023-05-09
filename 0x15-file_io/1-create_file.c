b10e93456bf2b6077a91f16080755488d0aa42cf
Isaac Frank <codingwithisaac@gmail.com>
2023-05-09 17:49:35 +0100

    Return -1 not 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t file, bytesWritten;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
 	bytesWritten = write(file, text_content, strlen(text_content));
