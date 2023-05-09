065e48dbb041783b56717e6f9a50b4e393a1f5b7
Isaac Frank <codingwithisaac@gmail.com>
2023-05-09 18:44:14 +0100

    Add TRUNC, CREATE and RW permission flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,29 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	bytesWritten = write(file, text_content, strlen(text_content));
 
 	close(file);
 	return ((file == -1 || bytesWritten == -1) ? -1 : 1);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
