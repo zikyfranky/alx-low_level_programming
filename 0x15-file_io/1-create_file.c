b3c7a285429b78d26de0610955fcf922ff3547b6
Isaac Frank <codingwithisaac@gmail.com>
2023-05-09 18:50:55 +0100

    Fix betty line over 80 characters

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,14 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t file, bytesWritten;
+	ssize_t file, bytesWritten, len = text_content ? strlen(text_content) : 0;
 
 	if (!filename)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	bytesWritten = write(file, text_content, text_content ? strlen(text_content) : 0);
+	bytesWritten = write(file, text_content, len);
 
 	close(file);
 	return ((file == -1 || bytesWritten == -1) ? -1 : 1);
